8c3a6d5684d9d4b780899761446cd031f05728bf
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-01-23 13:27:36 +0200

    Commit from TeleportHQ

---
+++ pages/test-page.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import { getEntityByAttribute } from '@teleporthq/cms-mappers/caisy'
+
+const TestPage = (props) => {
+  return (
+    <>
+      <div className="test-page-container">
+        <Head>
+          <title>test-page - Regional Research Agent</title>
+          <meta
+            property="og:title"
+            content="test-page - Regional Research Agent"
+          />
+        </Head>
+        <DataProvider
+          renderSuccess={(context_srqmpla) => (
+            <>
+              <h1 id={context_srqmpla?.id}>Heading</h1>
+            </>
+          )}
+          initialData={props.contextSrqmplaProp}
+          persistDataDuringLoading={true}
+          key={props?.contextSrqmplaProp?.id}
+        />
+      </div>
+      <style jsx>
+        {`
+          .test-page-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TestPage
+
+export async function getStaticProps(context) {
+  try {
+    const contextSrqmplaProp = await getEntityByAttribute({
+      ...context?.params,
+      projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+      query:
+        'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+      attribute: 'id',
+      id: '2',
+    })
+    return {
+      props: {
+        contextSrqmplaProp: contextSrqmplaProp?.data?.[0],
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}
